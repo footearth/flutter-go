b2cf844d4fcd878bc48f18a36388a80f2dfac65c
FooTearth <footearth@gmail.com>
2019-03-27 12:11:29 +0800

    themes/Cupertino/CupertinoApp

--- lib/widgets/themes/Cupertino/CupertinoApp/index.dart
+++ lib/widgets/themes/Cupertino/CupertinoApp/index.dart
@@ -3,9 +3,15 @@
 /// @Last Modified by: 一凨
 /// @Last Modified time: 2019-01-07 15:52:45
 
-import 'package:flutter/material.dart';
-
-import 'package:flutter_go/components/widget_demo.dart';
+import 'package:flutter/material.dart' show
+  StatefulWidget 
+, State
+, BuildContext
+, Widget
+;
+import 'package:flutter_go/components/widget_demo.dart' show
+  WidgetDemo
+;
 import './demo.dart' as demoBox;
 
 const String content0 = '''
@@ -13,7 +19,8 @@ const String content0 = '''
 > Cupertino 苹果设计风格的应用,用于创建 iOS 风格应用的顶层组件( Cupertino 苹果电脑的全球总公司所在地，位于美国旧金山 )
 - 它包含了 iOS 应用程序，通常需要的许多 widget;
 - 它的构建于基于 iOS 的 WidgetsApp 特定的默认值，如字体和物理滚动;
-''';
+'''
+;
 
 const String content1 = '''
 ### **基本用法**
@@ -29,25 +36,34 @@ const String content1 = '''
   - 通过反向滑动可以禁用页面；
   - 滚动到底或者顶，会触发 ios 风格的弹性效果；
   - 某些苹果字体系列在 Android上 不可用，可能导致未定义的字体提示。
-''';
+'''
+;
 
 class Demo extends StatefulWidget {
-  static const String routeName = '/themes/Cupertino/CupertinoApp';
-  _DemoState createState() => _DemoState();
+  static const String routeName =
+    '/themes/Cupertino/CupertinoApp'
+  ;
+  _DemoState createState() =>
+    _DemoState()
+  ;
 }
 
 class _DemoState extends State<Demo> {
+
   @override
-  Widget build(BuildContext context) {
-    return WidgetDemo(
+  Widget build(BuildContext context) =>
+    WidgetDemo(
       contentList: [
-        content0,
-        content1,
-        demoBox.CupertinoAppFullDefault()
-      ],
-      title: 'CupertinoApp',
-      docUrl: 'https://docs.flutter.io/flutter/cupertino/CupertinoApp-class.html',
-      codeUrl: 'themes/Cupertino/CupertinoApp/demo.dart',
-    );
-  }
+        content0
+      , content1
+      , demoBox.CupertinoAppFullDefault()
+      ]
+    , title: 'CupertinoApp'
+    , docUrl:
+        'https://docs.flutter.io/flutter/cupertino/CupertinoApp-class.html'
+    ,
+      codeUrl: 'themes/Cupertino/CupertinoApp/demo.dart'
+    )
+  ;
+
 }
--- lib/widgets/themes/Cupertino/index.dart
+++ lib/widgets/themes/Cupertino/index.dart
@@ -1,6 +1,11 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' show
+  BuildContext
+;
 
-import '../../../model/widget.dart';
+import '../../../model/widget.dart' show
+  WidgetPoint
+;
+
 import './CupertinoApp/index.dart' as CupertinoApp;
 import './CupertinoButton/index.dart' as CupertinoButton;
 import './CupertinoColors/index.dart' as CupertinoColors;
@@ -21,100 +26,138 @@ import './CupertinoTabView/index.dart' as CupertinoTabView;
 import './CupertinoTimerPicker/index.dart' as CupertinoTimerPicker;
 
 List<WidgetPoint> widgetPoints = [
-  WidgetPoint(
-    name: 'CupertinoPopupSurface',
-    routerName: CupertinoPopupSurface.Demo.routeName,
-    buildRouter: (BuildContext context) => CupertinoPopupSurface.Demo(),
-  ),
-  WidgetPoint(
-    name: 'CupertinoPicker',
-    routerName: CupertinoPicker.Demo.routeName,
-    buildRouter: (BuildContext context) => CupertinoPicker.Demo(),
-  ),
-  WidgetPoint(
-    name: 'CupertinoPageScaffold',
-    routerName: CupertinoPageScaffold.Demo.routeName,
-    buildRouter: (BuildContext context) => CupertinoPageScaffold.Demo(),
-  ),
-  WidgetPoint(
-    name: 'CupertinoPageRoute',
-    routerName: CupertinoPageRoute.Demo.routeName,
-    buildRouter: (BuildContext context) => CupertinoPageRoute.Demo(),
-  ),
-  WidgetPoint(
-    name: 'CupertinoNavigationBar',
-    routerName: CupertinoNavigationBar.Demo.routeName,
-    buildRouter: (BuildContext context) => CupertinoNavigationBar.Demo(),
-  ),
-  WidgetPoint(
-    name: 'CupertinoIcons',
-    routerName: CupertinoIcons.Demo.routeName,
-    buildRouter: (BuildContext context) => CupertinoIcons.Demo(),
-  ),
-  WidgetPoint(
-    name: 'CupertinoColors',
-    routerName: CupertinoColors.Demo.routeName,
-    buildRouter: (BuildContext context) => CupertinoColors.Demo(),
-  ),
-  WidgetPoint(
-    name: 'CupertinoButton',
-    routerName: CupertinoButton.Demo.routeName,
-    buildRouter: (BuildContext context) => CupertinoButton.Demo(),
-  ),
-  WidgetPoint(
-    name: 'CupertinoApp',
-    routerName: CupertinoApp.Demo.routeName,
-    buildRouter: (BuildContext context) => CupertinoApp.Demo(),
-  ),
-  WidgetPoint(
-    name: 'CupertinoScrollbar',
-    routerName: CupertinoScrollbar.Demo.routeName,
-    buildRouter: (BuildContext context) => CupertinoScrollbar.Demo(),
-  ),
-  WidgetPoint(
-    name: 'CupertinoSlider',
-    routerName: CupertinoSlider.Demo.routeName,
-    buildRouter: (BuildContext context) => CupertinoSlider.Demo(),
-  ),
-  WidgetPoint(
-    name: 'CupertinoSegmentedControl',
-    routerName: CupertinoSegmentedControl.Demo.routeName,
-    buildRouter: (BuildContext context) => CupertinoSegmentedControl.Demo(),
-  ),
-  WidgetPoint(
-    name: 'CupertinoSliverNavigationBar',
-    routerName: CupertinoSliverNavigationBar.Demo.routeName,
-    buildRouter: (BuildContext context) => CupertinoSliverNavigationBar.Demo(),
-  ),
-  WidgetPoint(
-    name: 'CupertinoSliverRefreshControl',
-    routerName: CupertinoSliverNavigationBar.Demo.routeName,
-    buildRouter: (BuildContext context) => CupertinoSliverNavigationBar.Demo(),
-  ),
-  WidgetPoint(
-    name: 'CupertinoSwitch',
-    routerName: CupertinoSwitch.Demo.routeName,
-    buildRouter: (BuildContext context) => CupertinoSwitch.Demo(),
-  ),
-  WidgetPoint(
-    name: 'CupertinoTabBar',
-    routerName: CupertinoTabBar.Demo.routeName,
-    buildRouter: (BuildContext context) => CupertinoTabBar.Demo(),
-  ),
-  WidgetPoint(
-    name: 'CupertinoTabScaffold',
-    routerName: CupertinoTabScaffold.Demo.routeName,
-    buildRouter: (BuildContext context) => CupertinoTabScaffold.Demo(),
-  ),
-  WidgetPoint(
-    name: 'CupertinoTabView',
-    routerName: CupertinoTabView.Demo.routeName,
-    buildRouter: (BuildContext context) => CupertinoTabView.Demo(),
-  ),
-  WidgetPoint(
-    name: 'CupertinoTimerPicker',
-    routerName: CupertinoTimerPicker.Demo.routeName,
-    buildRouter: (BuildContext context) => CupertinoTimerPicker.Demo(),
-  ),
-];
 
+  WidgetPoint(
+    name: 'CupertinoPopupSurface'
+  , routerName: CupertinoPopupSurface.Demo.routeName
+  , buildRouter: (BuildContext context) =>
+      CupertinoPopupSurface.Demo()
+  )
+,
+  WidgetPoint(
+    name: 'CupertinoPicker'
+  , routerName: CupertinoPicker.Demo.routeName
+  , buildRouter: (BuildContext context) =>
+      CupertinoPicker.Demo()
+  )
+,
+  WidgetPoint(
+    name: 'CupertinoPageScaffold'
+  , routerName: CupertinoPageScaffold.Demo.routeName
+  , buildRouter: (BuildContext context) =>
+      CupertinoPageScaffold.Demo()
+  )
+,
+  WidgetPoint(
+    name: 'CupertinoPageRoute'
+  , routerName: CupertinoPageRoute.Demo.routeName
+  , buildRouter: (BuildContext context) =>
+      CupertinoPageRoute.Demo()
+  )
+,
+  WidgetPoint(
+    name: 'CupertinoNavigationBar'
+  , routerName: CupertinoNavigationBar.Demo.routeName
+  , buildRouter: (BuildContext context) =>
+      CupertinoNavigationBar.Demo()
+  )
+,
+  WidgetPoint(
+    name: 'CupertinoIcons'
+  , routerName: CupertinoIcons.Demo.routeName
+  , buildRouter: (BuildContext context) =>
+      CupertinoIcons.Demo()
+  )
+,
+  WidgetPoint(
+    name: 'CupertinoColors'
+  , routerName: CupertinoColors.Demo.routeName
+  , buildRouter: (BuildContext context) =>
+      CupertinoColors.Demo()
+  )
+,
+  WidgetPoint(
+    name: 'CupertinoButton'
+  , routerName: CupertinoButton.Demo.routeName
+  , buildRouter: (BuildContext context) =>
+      CupertinoButton.Demo()
+  )
+,
+  WidgetPoint(
+    name: 'CupertinoApp'
+  , routerName: CupertinoApp.Demo.routeName
+  , buildRouter: (BuildContext context) =>
+      CupertinoApp.Demo()
+  )
+,
+  WidgetPoint(
+    name: 'CupertinoScrollbar'
+  , routerName: CupertinoScrollbar.Demo.routeName
+  , buildRouter: (BuildContext context) =>
+      CupertinoScrollbar.Demo()
+  )
+,
+  WidgetPoint(
+    name: 'CupertinoSlider'
+  , routerName: CupertinoSlider.Demo.routeName
+  , buildRouter: (BuildContext context) =>
+      CupertinoSlider.Demo()
+  )
+,
+  WidgetPoint(
+    name: 'CupertinoSegmentedControl'
+  , routerName: CupertinoSegmentedControl.Demo.routeName
+  , buildRouter: (BuildContext context) =>
+      CupertinoSegmentedControl.Demo()
+  )
+,
+  WidgetPoint(
+    name: 'CupertinoSliverNavigationBar'
+  , routerName: CupertinoSliverNavigationBar.Demo.routeName
+  , buildRouter: (BuildContext context) =>
+      CupertinoSliverNavigationBar.Demo()
+  )
+,
+  WidgetPoint(
+    name: 'CupertinoSliverRefreshControl'
+  , routerName: CupertinoSliverNavigationBar.Demo.routeName
+  , buildRouter: (BuildContext context) =>
+      CupertinoSliverNavigationBar.Demo()
+  )
+,
+  WidgetPoint(
+    name: 'CupertinoSwitch'
+  , routerName: CupertinoSwitch.Demo.routeName
+  , buildRouter: (BuildContext context) =>
+      CupertinoSwitch.Demo()
+  )
+,
+  WidgetPoint(
+    name: 'CupertinoTabBar'
+  , routerName: CupertinoTabBar.Demo.routeName
+  , buildRouter: (BuildContext context) =>
+      CupertinoTabBar.Demo()
+  )
+,
+  WidgetPoint(
+    name: 'CupertinoTabScaffold'
+  , routerName: CupertinoTabScaffold.Demo.routeName
+  , buildRouter: (BuildContext context) =>
+      CupertinoTabScaffold.Demo()
+  )
+,
+  WidgetPoint(
+    name: 'CupertinoTabView'
+  , routerName: CupertinoTabView.Demo.routeName
+  , buildRouter: (BuildContext context) =>
+      CupertinoTabView.Demo()
+  )
+,
+  WidgetPoint(
+    name: 'CupertinoTimerPicker'
+  , routerName: CupertinoTimerPicker.Demo.routeName
+  , buildRouter: (BuildContext context) =>
+      CupertinoTimerPicker.Demo()
+  )
+]
+;
